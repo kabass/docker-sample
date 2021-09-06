9bf0f7a620b6ea32c995d4fe1a8b71f1ba8cc658
Bassirou <ka.bassirou@gmail.com>
2021-09-06 23:12:36 +0200

    adding node port service

--- kubernetes/case-1-2-pod-pv/command.sh
+++ kubernetes/case-1-2-pod-pv/command.sh
@@ -1,3 +1,3 @@
 kubectl $1 -f kubernetes/case-1-2-pod-pv/pv.yaml
-kubectl $1 -f kubernetes/case-1-2-pod-pv/pvd.yaml
+kubectl $1 -f kubernetes/case-1-2-pod-pv/pv.yaml
 kubectl $1 -f kubernetes/case-1-2-pod-pv/pod.yaml
