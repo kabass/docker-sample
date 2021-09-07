d589159a7e9d81bc2e81b383370836e8650a4dac
Bassirou <ka.bassirou@gmail.com>
2021-09-07 10:59:48 +0200

    adding node port service

--- kubernetes/case-6-netpol/command.sh
+++
@@ -1,4 +0,0 @@
-kubectl $1 -f kubernetes/case-6-netpol/deployment.yaml
-kubectl $1 -f kubernetes/case-6-netpol/service.yaml
-kubectl $1 -f kubernetes/case-6-netpol/pod-front.yaml
-kubectl $1 -f kubernetes/case-6-netpol/service-front.yaml
